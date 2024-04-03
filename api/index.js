43a81d2457939902ae0ec5034ef5ab1db9ce7026
Rishi Kejriwal <rishikejriwalrk@gmail.com>
2024-04-03 23:11:24 +0530

    ChatUI added

--- api/index.js
+++ api/index.js
@@ -45,7 +45,7 @@ app.get("/profile", async (req, res) => {
 	const token = req.cookies?.token;
 	if (token) {
 		jwt.verify(token, jwtSecret, {}, (err, userData) => {
-			if (err) throw err;
+			// if (err) throw err;
 			res.json(userData);
 		});
 	} else {
